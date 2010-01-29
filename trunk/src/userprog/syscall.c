14cfb155d92ce09d4a6dd55c56b9a778fca8e818
yinfeng.qin@gmail.com <yinfeng.qin@gmail.com@2f2d7c58-0c32-11df-8c7c-8529219665b1>
2010-01-29 06:14:09 +0000

    added infrastructure for syscall

--- trunk/src/userprog/syscall.c
+++ trunk/src/userprog/syscall.c
@@ -6,6 +6,9 @@
 
 static void syscall_handler (struct intr_frame *);
 
+/* yinfeng ******************************************************************/
+static uint32_t pop (struct intr_frame *f);
+
 void
 syscall_init (void) 
 {
@@ -13,8 +16,156 @@ syscall_init (void)
 }
 
 static void
-syscall_handler (struct intr_frame *f UNUSED) 
+syscall_handler (struct intr_frame *f) 
 {
-  printf ("system call!\n");
-  thread_exit ();
+  /* get syscall number */
+  int syscall_no = (int)(pop (f));
+
+  /* dispatch to individual calls */
+  uint32_t arg1;
+  uint32_t arg2;
+  uint32_t arg3;
+  switch (syscall_no)
+    {
+      case SYS_HALT:
+        halt ();
+        break;
+      case SYS_EXIT:
+        arg1 = pop (f);
+        exit ((int)arg1);
+        break;
+      case SYS_EXEC:
+        arg1 = pop (f);
+        f->eax = (uint32_t)exec ((const char*)arg1);
+        break;
+      case SYS_WAIT:
+        arg1 = pop (f);
+        f->eax = (uint32_t)wait ((int)arg1);
+        break;
+      case SYS_CREATE:
+        arg1 = pop (f);
+        arg2 = pop (f);
+        f->eax = (uint32_t)create ((const char*)arg1, (unsigned)arg2);
+        break;
+      case SYS_REMOVE:
+        arg1 = pop (f);
+        f->eax = (uint32_t)remove ((const char*)arg1);
+        break;
+      case SYS_OPEN:
+        arg1 = pop (f);
+        f->eax = (uint32_t)open ((const char*)arg1);
+        break;
+      case SYS_FILESIZE:
+        arg1 = pop (f);
+        f->eax = (uint32_t)filesize ((int)arg1);
+        break;
+      case SYS_READ:
+        arg1 = pop (f);
+        arg2 = pop (f);
+        arg3 = pop (f);
+        f->eax = (uint32_t)read ((int)arg1, (void*)arg2, (unsigned)arg3);
+        break;
+      case SYS_WRITE:
+        arg1 = pop (f);
+        arg2 = pop (f);
+        arg3 = pop (f);
+        f->eax = (uint32_t)write((int)arg1, (const void*)arg2, (unsigned)arg3);
+        break;
+      case SYS_SEEK:
+        arg1 = pop (f);
+        arg2 = pop (f);
+        seek ((int)arg1, (unsigned)arg2);
+        break;
+      case SYS_TELL:
+        arg1 = pop (f);
+        f->eax = (uint32_t)tell ((int)arg1);
+        break;
+      case SYS_CLOSE:
+        arg1 = pop (f);
+        close ((int)arg1);
+        break;
+      default:
+        /* we should throw exception here indicating no specified operation */
+        break;
+    }
 }
+
+static uint32_t pop (struct intr_frame *f)
+{
+  uint32_t result = *(uint32_t *)f->esp;
+  f->esp += 4;
+
+  return result;
+}
+
+void
+halt (void)
+{}
+
+void
+exit (int status)
+{}
+
+pid_t
+exec (const char *cmd_line)
+{
+  return 0;
+}
+
+int
+wait (pid_t pid)
+{
+  return 0;
+}
+
+bool
+create (const char *file, unsigned initial_size)
+{
+  return false;
+}
+
+bool
+remove (const char *file)
+{
+  return true;
+}
+
+int
+open (const char *file)
+{
+  return 0;
+}
+
+int
+filesize (int fd)
+{
+  return 0;
+}
+
+int
+read (int fd, void *buffer, unsigned size)
+{
+  return 0;
+}
+
+int
+write (int fd, const void *buffer, unsigned size)
+{
+  return 0;
+}
+
+void
+seek (int fd, unsigned position)
+{}
+
+unsigned
+tell (int fd)
+{
+  return 0;
+}
+
+void
+close (int fd)
+{}
+
+/* yinfeng ******************************************************************/
