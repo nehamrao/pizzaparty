3da7889a40db1011f35f49347710eac0928ae1b2
yinfeng.qin@gmail.com <yinfeng.qin@gmail.com@2f2d7c58-0c32-11df-8c7c-8529219665b1>
2010-01-30 21:13:57 +0000

    corrected pop in syscall.c

--- trunk/src/userprog/process.c
+++ trunk/src/userprog/process.c
@@ -90,6 +90,12 @@ start_process (void *file_name_)
   if_.eflags = FLAG_IF | FLAG_MBS;
   success = load (file_name, &if_.eip, &if_.esp);
 
+/* yinfeng *******************************************************************/
+  struct thread* t = thread_current ();
+  t->child_load_success = success;
+  sema_up (&t->sema_child_load);
+/* yinfeng *******************************************************************/
+
   /* If load failed, quit. */
   palloc_free_page (file_name);
   if (!success) 
