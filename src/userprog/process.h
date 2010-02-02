884e0f14c4925a26c1cb832caf660fb73ffafb47
gxj826 <gxj826@2f2d7c58-0c32-11df-8c7c-8529219665b1>
2010-02-02 09:38:02 +0000

    4 failed

--- src/userprog/process.h
+++ src/userprog/process.h
@@ -7,6 +7,5 @@ tid_t process_execute (const char *file_name);
 int process_wait (tid_t);
 void process_exit (void);
 void process_activate (void);
-bool checkvaddr(const void * vaddr);
 
 #endif /* userprog/process.h */
