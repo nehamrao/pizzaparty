6556bd3b05196a528b57f80b1a3e4abb5785a456
gxj826 <gxj826@2f2d7c58-0c32-11df-8c7c-8529219665b1>
2010-03-02 20:13:53 +0000

    Cache, not done yet

---
+++ src/filesys/cache.h
@@ -0,0 +1,31 @@
+//cache.h
+#include "off_t.h"
+#include "threads/synch.h"
+#include "devices/block.h"
+
+struct shared_lock
+{
+  int i;
+  struct lock lock;
+  struct condition cond;
+};
+
+struct cache_block
+{
+  block_sector_t sector_no;
+  bool dirty;
+  unsigned int time_stamp;
+  bool valid;
+  struct shared_lock shared_lock;
+  void *data;
+};
+
+void cache_init (void);
+struct cache_block *cache_get (block_sector_t sector_no);
+void cache_read ( struct cache_block *cb, void *data, off_t ofs, int length);
+void cache_write ( struct cache_block *cb, void *data, off_t ofs, int length);
+bool blkcmp (uint8_t *a, uint8_t *b, int length);
+void acquire_shared (struct shared_lock *s);
+void acquire_exclusive (struct shared_lock *s);
+void release_shared (struct shared_lock *s);
+void release_exclusive (struct shared_lock *s);
